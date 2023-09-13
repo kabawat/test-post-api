27eb11e7f523ab3e72f24abef9a619be24a3f217
w3App <kshatriyakabawat@gmail.com>
2023-09-13 10:52:04 +0530

    Update index.js

--- middleware/email/index.js
+++ middleware/email/index.js
@@ -1,10 +1,10 @@
 module.exports.emailValid = (req, res, next) => {
     try {
-        const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+        // const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
-        const { email } = req?.body
-        const isValidEmail = emailRegex.test(email)
-        if (!isValidEmail) {
+        // const { email } = req?.body
+        // const isValidEmail = emailRegex.test(email)
+        if (!email) {
             throw new Error("invalid Email!")
         }
         next()
