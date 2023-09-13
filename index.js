f7adbe9a8f7e9bf8726a4a2d4af8d11d54ffd5cd
w3App <kshatriyakabawat@gmail.com>
2023-09-13 12:05:51 +0530

    file Upload

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const exp = require('express')
+var bodyParser = require('body-parser')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const app = exp();
 const router = require('./router/')
-app.use(exp.json())
-app.use(exp.urlencoded({ extended: true }))
+// app.use(exp.json())
+// app.use(exp.urlencoded({ extended: true }))
 app.use(cors())
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload())
 app.use(router)
+
+
 app.post('/login', function (req, res) {
     const num = Math.random()
     const token = Math.floor(num * 1000000)
