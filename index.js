161c275e87bbdbd670f6c551646b0cfac8e680da
kabawat <118162167+kabawat@users.noreply.github.com>
2024-04-17 09:13:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./router/')
 app.use(exp.json())
 app.use(exp.urlencoded({ extended: true }))
 app.use(cors(
-    { origin: ['*', 'http://127.0.0.1:5500/', "http://127.0.0.1:5500/", 'http://localhost:5500/'] }
+    { origin: ['*', 'http://127.0.0.1:5500/', "http://127.0.0.1:5500/", 'http://localhost:5500/', 'http://localhost:3000', 'http://localhost:3001'] }
 ))
 // app.use('/user', exp.static(path.join(__dirname, 'public/user')))
 app.use(router)
